839efc858a08725da4a30d84e5a6a2592d2388c9
Einars Vilnis <vilniseinars@gmail.com>
2019-07-04 22:59:04 +0100

    update

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,79 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './Styles'
 import { Form, Field } from 'react-final-form'
+import arrayMutators from 'final-form-arrays'
+import { FieldArray } from 'react-final-form-arrays'
 
 const MyForm = props => {
   const onSubmit = values => {
-    props.updateValues(values)
+    console.log('var', values.confirm.initialValues(''))
   }
+
   return (
     <Styles>
-      <h1>🏁 React Final Form Example</h1>
-      <h2>Synchronous Record-Level Validation</h2>
+      <h1>🏁 React Final Form - Array Fields</h1>
       <a href='https://github.com/erikras/react-final-form#-react-final-form'>
         Read Docs
       </a>
       <Form
         onSubmit={onSubmit}
-        validate={values => {
-          const errors = {}
-          if (!values.username) {
-            errors.username = 'Required'
-          }
-          if (!values.password) {
-            errors.password = 'Required'
-          }
-          if (!values.confirm) {
-            errors.confirm = 'Required'
-          } else if (values.confirm !== values.password) {
-            errors.confirm = 'Must match'
-          }
-          return errors
+        mutators={{
+          ...arrayMutators
         }}
-        render={({ handleSubmit, reset, submitting, pristine, values }) => (
-          <form onSubmit={handleSubmit}>
-            <Field name='username'>
-              {({ input, meta }) => (
-                <div>
-                  <label>Username</label>
-                  <input {...input} type='text' placeholder='Username' />
-                  {meta.error && meta.touched && <span>{meta.error}</span>}
-                </div>
-              )}
-            </Field>
-            <Field name='password'>
-              {({ input, meta }) => (
-                <div>
-                  <label>Password</label>
-                  <input {...input} type='password' placeholder='Password' />
-                  {meta.error && meta.touched && <span>{meta.error}</span>}
-                </div>
-              )}
-            </Field>
-            <Field name='confirm'>
-              {({ input, meta }) => (
-                <div>
-                  <label>Confirm</label>
-                  <input {...input} type='password' placeholder='Confirm' />
-                  {meta.error && meta.touched && <span>{meta.error}</span>}
-                </div>
-              )}
-            </Field>
-            <div className='buttons'>
-              <button type='submit' disabled={submitting}>
-                Submit
-              </button>
-              <button
-                type='button'
-                onClick={reset}
-                disabled={submitting || pristine}
-              >
-                Reset
-              </button>
-            </div>
-            <pre>{JSON.stringify(values, 0, 2)}</pre>
-          </form>
-        )}
+        render={({
+          handleSubmit,
+          form: {
+            mutators: { push, pop }
+          }, // injected from final-form-arrays above
+          pristine,
+          form,
+          submitting,
+          values
+        }) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label>Company</label>
+                <Field name='company' component='input' />
+              </div>
+              <div className='buttons'>
+                <button
+                  type='button'
+                  onClick={() => push('customers', undefined)}
+                >
+                  Add Customer
+                </button>
+                <button type='button' onClick={() => pop('customers')}>
+                  Remove Customer
+                </button>
+              </div>
+              <FieldArray name='customers'>
+                {({ fields }) =>
+                  fields.map((name, index) => (
+                    <div key={name}>
+                      <label>First Name </label>
+                      <Field
+                        name={`${name}.firstName`}
+                        component='input'
+                        type='checkbox'
+                        placeholder='First Name'
+                      />
+                      <br />
+                      <label>Last Name </label>
+                      <Field
+                        name={`${name}.lastName`}
+                        component='input'
+                        placeholder='Last Name'
+                        type='checkbox'
+                      />
+                      <span
+                        onClick={() =>
+                          fields.update(index, {
+                            firstName: 'hello',
+                            lastName: 'hello'
+                          })
+                        }
+                        style={{ cursor: 'pointer' }}
+                      >
+                        ❌
+                      </span>
+                      <span
+                        onClick={() =>
+                          fields.update(index, {
+                            firstName: '',
+                            lastName: ''
+                          })
+                        }
+                        style={{ cursor: 'pointer' }}
+                      >
+                        clear
+                      </span>
+                    </div>
+                  ))
+                }
+              </FieldArray>
+
+              <div className='buttons'>
+                <button type='submit' disabled={submitting || pristine}>
+                  Submit
+                </button>
+                <button
+                  type='button'
+                  onClick={form.reset}
+                  disabled={submitting || pristine}
+                >
+                  Reset
+                </button>
+              </div>
+              <pre>{JSON.stringify(values, 0, 2)}</pre>
+            </form>
+          )
+        }}
       />
     </Styles>
   )
